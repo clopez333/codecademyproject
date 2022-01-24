d9a5b8ec5d0778aac4ed151694d6853b50a9f61b
clopez333 <97922177+clopez333@users.noreply.github.com>
2022-01-24 15:48:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const Randomize = () => { 
 // messages and numbers
-let messages = ['Success is a lousy teacher. It seduces smart people into thinking they can/'t succeed - Bill Gates', 
-'Your time is limited, so don't waste it living someone else/'s life- Steve Jobs', 
+let messages = ['Success is a lousy teacher. It seduces smart people into thinking they can\'t succeed- Bill Gates', 
+'Your time is limited, so don\'t waste it living someone else\'s life- Steve Jobs', 
 'Be humble in victory or defeat- Conor McGregor', 'I like criticism. It makes me stronger- LeBron James', ]; 
 let numbers = [1,2,3,4,5,6,7,8,9];
 
@@ -10,7 +10,7 @@ let randomMsg = Math.floor(Math.random() * messages.length);
 let randomNum = Math.floor(Math.random() * numbers.length);
 
 //outputs the random end result to the console 
-let output = []; output.push('Your motivational quote is: " + messages[randomMsg] + " and your number is: ' + numbers[randomNum]); 
+let output = []; output.push('Your motivational quote is: ' + messages[randomMsg] + ' and your number is: ' + numbers[randomNum]); 
 return output };
 
 console.log(Randomize());
